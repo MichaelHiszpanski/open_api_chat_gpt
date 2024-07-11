b5794776a448906013010d4c4a1cfc21cb5e3579
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-07-11 15:33:43 +0100

    first commit

---
+++ lib/chat_gpt/chat_gpt_screen.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:open_api_chat_gpt/chat_gpt/feature_box.dart';
+import 'package:open_api_chat_gpt/chat_gpt/pallete.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+class ChatGPTScreen extends StatefulWidget {
+  const ChatGPTScreen({super.key});
+
+  @override
+  State<ChatGPTScreen> createState() => _ChatGPTScreenState();
+}
+
+class _ChatGPTScreenState extends State<ChatGPTScreen> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+  final flutterTts = FlutterTts();
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+    initTextToSpeech();
+  }
+
+  Future<void> initTextToSpeech() async {
+    await flutterTts.setSharedInstance(true);
+    setState(() {});
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  Future<void> systemSpeak(String content) async {
+    await flutterTts.speak(content);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+    flutterTts.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Allen"),
+          leading: const Icon(Icons.menu),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      margin: const EdgeInsets.only(top: 4),
+                      decoration: const BoxDecoration(
+                          color: Pallete.assistantCircleColor,
+                          shape: BoxShape.circle),
+                    ),
+                  ),
+                  Container(
+                    height: 123,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/virtualAssistant.png"))),
+                  )
+                ],
+              ),
+              //chat bubble
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                margin: const EdgeInsets.symmetric(horizontal: 40)
+                    .copyWith(top: 30),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Pallete.blackColor),
+                    borderRadius: BorderRadius.circular(20).copyWith(
+                      topLeft: Radius.zero,
+                    )),
+                child: const Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    "Good morning, what task can I do for you?",
+                    style: TextStyle(
+                        color: Pallete.mainFontColor, fontFamily: "Cera Pro"),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(top: 10, left: 22),
+                child: const Text(
+                  "Here are a few features",
+                  style: TextStyle(
+                      fontFamily: "Cera Pro",
+                      color: Pallete.mainFontColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              //suggestion list
+              const Column(
+                children: [
+                  FeatureBox(
+                    color: Pallete.firstSuggestionBoxColor,
+                    headerText: "chatGpt",
+                    descriptionText:
+                        "Performing hot restart..3.0sRestarted application in 3,046ms.No observables detected in the build method of Observer",
+                  ),
+                  FeatureBox(
+                    color: Pallete.secondSuggestionBoxColor,
+                    headerText: "Dall-E",
+                    descriptionText:
+                        "Performing hot restart..3.0sRestarted application in 3,046ms.No observables detected in the build method of Observer",
+                  ),
+                  FeatureBox(
+                    color: Pallete.thirdSuggestionBoxColor,
+                    headerText: "Smart Voice Assistant",
+                    descriptionText:
+                        "Performing hot restart..3.0sRestarted application in 3,046ms.No observables detected in the build method of Observer",
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Pallete.firstSuggestionBoxColor,
+          onPressed: () async {
+            if (await speechToText.hasPermission &&
+                speechToText.isNotListening) {
+              await startListening();
+            } else if (speechToText.isListening) {
+              await systemSpeak(lastWords);
+              await stopListening(); //stop when on
+            } else {
+              initSpeechToText();
+            }
+          },
+          child: const Icon(Icons.mic),
+        ));
+  }
+}
